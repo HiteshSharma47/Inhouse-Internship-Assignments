862b6eabbd25c98e8deddff71b4e0b6fe380a1d6
Hitesh Sharma <hs4245742@gmail.com>
2026-06-13 09:15:23 +0530

    Updated Assignment(SQL)

--- Case_Study_2(Data_Bank).sql
+++ Case_Study_2(Data_Bank).sql
@@ -72,7 +72,50 @@ WITH (
 
     -- 4).How many days on average are customers reallocated to a different node?
 
+            USE data_bank
+
+            ;WITH reallocation_days AS (
+            SELECT
+                customer_id,
+                DATEDIFF(day, start_date, end_date) AS days_allocated
+            FROM customer_nodes
+            WHERE end_date <> '9999-12-31'
+            )
+            SELECT
+                AVG(CONVERT(decimal(10,2), days_allocated)) AS avg_reallocation_days
+            FROM reallocation_days;
+
     -- 5).What is the median, 80th and 95th percentile for this same reallocation days metric for each region?
+
+            USE data_bank
+
+            ;WITH reallocation_days AS (
+            SELECT
+                r.region_name,
+                DATEDIFF(day, cn.start_date, cn.end_date) AS days_allocated
+            FROM customer_nodes cn
+            JOIN regions r
+                ON cn.region_id = r.region_id
+            WHERE cn.end_date <> '9999-12-31'
+            )
+
+            SELECT DISTINCT
+                region_name,
+            
+                PERCENTILE_CONT(0.5)
+                    WITHIN GROUP (ORDER BY days_allocated)
+                    OVER (PARTITION BY region_name) AS median_days,
+            
+                PERCENTILE_CONT(0.8)
+                    WITHIN GROUP (ORDER BY days_allocated)
+                    OVER (PARTITION BY region_name) AS percentile_80_days,
+            
+                PERCENTILE_CONT(0.95)
+                    WITHIN GROUP (ORDER BY days_allocated)
+                    OVER (PARTITION BY region_name) AS percentile_95_days
+            
+            FROM reallocation_days
+            ORDER BY region_name;
 
 
 -- B. Customer Transactions --
@@ -86,6 +129,7 @@ WITH (
     -- 2).What is the average total historical deposit counts and amounts for all customers?
 
             USE data_bank
+
             SELECT
             AVG(total_deposits) as avg_total_deposits,
             AVG(total_amount) as avg_amount
@@ -101,5 +145,32 @@ WITH (
             ) t
 
     -- 3).For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
+
+            USE data_bank
+            
+            ;WITH monthly_transactions AS (
+            SELECT
+            customer_id,
+            MONTH(txn_date) AS month_num,
+
+            SUM(CASE WHEN txn_type = 'deposit' THEN 1 ELSE 0 END) AS deposit_count,
+            SUM(CASE WHEN txn_type = 'purchase' THEN 1 ELSE 0 END) AS purchase_count,
+            SUM(CASE WHEN txn_type = 'withdrawal' THEN 1 ELSE 0 END) AS withdrawal_count
+            FROM customer_transactions
+            GROUP BY
+            customer_id,
+            MONTH(txn_date)
+            )
+
+            SELECT
+            month_num,
+            COUNT(*) AS customer_count
+            FROM monthly_transactions
+            WHERE deposit_count > 1
+            AND (purchase_count >= 1 OR withdrawal_count >= 1)
+            GROUP BY month_num
+            ORDER BY month_num;
+
+
 
 
